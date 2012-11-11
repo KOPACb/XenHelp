00e77277cbc67e3dff8769a680cf0e14f302bb4c
The KOPACb <KOPACb@gmail.com>
2012-11-12 03:32:42 +0400

    .

--- autostart.py
+++ autostart.py
@@ -17,6 +17,36 @@ def get_list():
     plain_list = open('list','r')
     return plain_list.readlines()
 
+def get_detail(uuid):
+    '''get details of VM
+    '''
+    plain_detail = open(uuid,'r')
+    autostart = False
+    backup = False
+    migrate = False
+    for line in plain_detail:
+        try:
+            field , value = line.split(':')
+            if field.find('name-label') != -1:
+                name = value.strip()
+            if field.find('power-state') != -1:
+                state = value.strip()
+            if field.find('tags') != -1:
+                tags = value.strip()
+                if tags.find('autostart') != -1:
+                    autostart = True
+                if tags.find('backup') != -1:
+                    backup = True
+                if tags.find('migrate') != -1:
+                    migrate = True
+            else:
+                continue
+        except ValueError: continue
+    result = dict(uuid=uuid, name=name, state=state, autostart=autostart, backup=backup, migrate=migrate, tags=tags)
+    return result
+
+
+
 def readlog(p_list):
     '''format list vms with uuid:%uuid,name:%name,state:%state
     '''
@@ -46,28 +76,27 @@ def read_uuid(p_list):
             else:
                 continue
         except ValueError: continue
-        yield uuid.strip()
+        yield uuid
     result = read_uuid(p_list)
     return result
 
 
-def formatting(list):
+def formatting(p_list):
     i = 0
-    for line in list:
+    for uuid in p_list:
         try:
-            f, value = line.split(':')
-
+            lst = dict(uuid=get_detail(uuid))
+            i += 1
         except ValueError: continue
 
-
+    return lst
 def main():
     '''main unit'''
     p_list = get_list()
-    l = print(len(p_list))
-
     log = list(read_uuid(p_list))
     print(log)
-
+    data = formatting(log)
+    print(data)
 
 
 main()
